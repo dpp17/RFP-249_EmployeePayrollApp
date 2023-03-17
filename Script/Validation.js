dd1f1adb9ae395e904c50fbb14f43c6ae2111c5d
dpp17 <durga123q.com>
2023-03-17 06:15:32 +0530

    Added:event listener added for salary range value

---
+++ Script/Validation.js
@@ -0,0 +1,6 @@
+const value = document.querySelector(".salary-output");
+const input = document.querySelector("#salary");
+value.textContent = input.value;
+input.addEventListener("input", (event) => {
+value.textContent = event.target.value
+});
